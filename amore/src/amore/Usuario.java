8da93a15401650bd9bc7489b08543b7c26f8fb3c
vickaval <131685935+vickaval@users.noreply.github.com>
2023-05-02 00:39:19 -0300

    Update Usuario.java

--- amore/src/amore/Usuario.java
+++ amore/src/amore/Usuario.java
@@ -1,5 +1,7 @@
 package amore;
 
+import javax.swing.JOptionPane;
+
 public abstract class Usuario {
 	
 	private String nombre;
@@ -116,6 +118,49 @@ public abstract class Usuario {
 		return "Usuario [nombre=" + nombre + ", apellido=" + apellido + ", id=" + id + ", nombreArea=" + nombreArea
 				+ ", aniosAntiguedad=" + aniosAntiguedad + ", telefono=" + telefono + ", usuario=" + usuario
 				+ ", contraseña=" + contraseña + ", sueldo=" + sueldo + "]";
+		}
+		
+		
+		//metodos
+		public boolean iniciarSesion(Usuario u) {
+			
+			String []usuario= {"Cliente" , "Administrador","Operario","Almacenista", "Vendedor"};
+	        int usu = JOptionPane.showOptionDialog(null, "Elija una opcion", "Mi pantalla de pregunta",JOptionPane.DEFAULT_OPTION,
+	                JOptionPane.QUESTION_MESSAGE, null, usuario, usuario[0]);
+	        
+	        
+	        String pass = (String)JOptionPane.showInputDialog(null, "Ingrese su contraseña: ");
+	        //numero maximo de caracteres de contraseña = 8
+	        if(pass.length() <= 8) {
+	        		    
+			if(pass.equals(getContraseña())) {
+			
+			//mensaje de confirmacion por tipo de usuario
+	            if (usu == 0) {
+	            System.out.println("Bienvenido a Amore Pastas");
+	            } else  if (usu == 1) {
+	                System.out.println("Bienvenido Administrador");
+	            } else  if (usu == 2) {
+	                System.out.println("Bienvenido Operario");
+	            } else  if (usu == 3) {
+	            	System.out.println("Bienvenido Almacenista");
+	            } else if (usu == 4) {
+	            	System.out.println("Bienvenido Vendedor");
+	            }
+	        
+	        //inicio sesion (usuario y contaseña correcta)
+	    	return true;
+			} else {
+				
+		
+			}
+			
+	        }//no pudo iniciar sesion
+			return false;
+	        
+	        
+	        
+	        
 		}
 	
 	}
