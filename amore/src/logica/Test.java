49d342c21d39a8d38a1e14f2265ba4cdebc6b6e8
MartinaAsad <lasupermartu15@gmail.com>
2023-05-02 21:17:10 -0300

    Update Test.java

--- amore/src/logica/Test.java
+++ amore/src/logica/Test.java
@@ -2,7 +2,7 @@ package logica;
 
 public class Test {
     public static void main(String[] args) {
-        Usuario u =new Usuario("Martina", "Asad", 456, "Ventas", 5, 123654, "masad", "martina1990", 45896);
+        Usuario u =new Cliente("Martina", "Asad", 45678955,"masad", "martina1990", 45896);
         u.iniciarSesion(u);
     }
 }
