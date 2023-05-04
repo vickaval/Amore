438f30a9264a07768ea9991a02d4189c509865e0
vickaval <131685935+vickaval@users.noreply.github.com>
2023-05-04 18:18:06 -0300

    listo

--- amore/src/amore/MateriaPrima.java
+++ amore/src/amore/MateriaPrima.java
@@ -1,80 +1,72 @@
-package Logica;
-
-import java.util.ArrayList;
+package amore;
 
 public class MateriaPrima {
-	private String nombre;
-	private String procedencia;
-	private double precio;
-	private int stockDisponible;
-	private int codigoMp;
-	ArrayList<MateriaPrima> nombreYPrecioMateriaPrima;
-	
-	MateriaPrima(){
 		
-	}
-
-	public MateriaPrima(String nombre, String procedencia, double precio, int stockDisponible, int codigoMp,
-			ArrayList<MateriaPrima> nombreYPrecioMateriaPrima) {
+		private int codigoMp;
+		private String nombre;
+		private String procedencia;
+		private double precio;
+		private int stockDisponible;
 		
-		this.nombre = nombre;
-		this.procedencia = procedencia;
-		this.precio = precio;
-		this.stockDisponible = stockDisponible;
-		this.codigoMp = codigoMp;
-		this.nombreYPrecioMateriaPrima = nombreYPrecioMateriaPrima;
-	}
+		
+		
+		MateriaPrima(){
+			
+		}
 
-	public String getNombre() {
-		return nombre;
-	}
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
 
-	public String getProcedencia() {
-		return procedencia;
-	}
+		public MateriaPrima(int codigoMp, String nombre, String procedencia, double precio, int stockDisponible) {
+			super();
+			this.codigoMp = codigoMp;
+			this.nombre = nombre;
+			this.procedencia = procedencia;
+			this.precio = precio;
+			this.stockDisponible = stockDisponible;
+		}
 
-	public void setProcedencia(String procedencia) {
-		this.procedencia = procedencia;
-	}
 
-	public double getPrecio() {
-		return precio;
-	}
+		public String getNombre() {
+			return nombre;
+		}
 
-	public void setPrecio(double precio) {
-		this.precio = precio;
-	}
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
 
-	public int getStockDisponible() {
-		return stockDisponible;
-	}
+		public String getProcedencia() {
+			return procedencia;
+		}
 
-	public void setStockDisponible(int stockDisponible) {
-		this.stockDisponible = stockDisponible;
-	}
+		public void setProcedencia(String procedencia) {
+			this.procedencia = procedencia;
+		}
 
-	public int getCodigoMp() {
-		return codigoMp;
-	}
+		public double getPrecio() {
+			return precio;
+		}
 
-	public void setCodigoMp(int codigoMp) {
-		this.codigoMp = codigoMp;
-	}
+		public void setPrecio(double precio) {
+			this.precio = precio;
+		}
 
-	public ArrayList<MateriaPrima> getNombreYPrecioMateriaPrima() {
-		return nombreYPrecioMateriaPrima;
-	}
+		public int getStockDisponible() {
+			return stockDisponible;
+		}
 
-	public void setNombreYPrecioMateriaPrima(ArrayList<MateriaPrima> nombreYPrecioMateriaPrima) {
-		this.nombreYPrecioMateriaPrima = nombreYPrecioMateriaPrima;
-	}
-	
-	
-	
+		public void setStockDisponible(int stockDisponible) {
+			this.stockDisponible = stockDisponible;
+		}
+
+		public int getCodigoMp() {
+			return codigoMp;
+		}
+
+		public void setCodigoMp(int codigoMp) {
+			this.codigoMp = codigoMp;
+		}
+
+
 	
 
 }
