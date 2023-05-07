b3243d977f076573533b09113411ced49298fcc5
MartinaAsad <lasupermartu15@gmail.com>
2023-05-07 14:06:19 -0300

    Clase lista

--- amore/src/logica/Cliente.java
+++ amore/src/logica/Cliente.java
@@ -44,10 +44,47 @@ public class Cliente extends Usuario{
 
 
     //metodo inicar sesion esta en  usuario, lo modifique para que aparezca cliente
-    public void realizarPedido(){
-        //faltan clases para hacer correctamente el metodo como Pedido
-        System.out.println("Bienvenido "+getUsuario+" seleccione lo que va a pedir y la cantidad: ");
-        
+
+    public void seleccionarProducto(ArrayList<Producto> lista,Pedido p) {    
+    	for (int i = 0; i < lista.size(); i++) {
+			System.out.println(lista.get(i).getNombre());
+			System.out.println(lista.get(i).getPrecio());
+		}
+    	
+    	 Object[] productos = lista.toArray();
+    	 int cantidad=0;
+    	 double total=0;
+    	 int opciones=JOptionPane.showOptionDialog(null, "Elija una opcion", "Mi pantalla de pregunta", JOptionPane.DEFAULT_OPTION,
+ 	            JOptionPane.QUESTION_MESSAGE, null, productos, productos[0]);
+    	 if(opciones<productos.length) {
+    		 cantidad=Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la cantidad a llevar: "));
+    		 if(cantidad<=lista.get(opciones).getStockDisponible()) {
+    			 total=lista.get(opciones).getPrecio()*cantidad;
+    			 JOptionPane.showMessageDialog(null, "El total de la compra es: "+total);
+    			
+    		 }else {
+    			 JOptionPane.showMessageDialog(null, "Stock insuficiente");
+    		 }
+    	 }
+    	
+    	
+    }
+    
+    public void metodosDePago(Pedido p) {
+    	String []mediosDePago= {"Efectivo", "Transferencia"};
+    	int pago = JOptionPane.showOptionDialog(null, "Elija un medio de pago", "Mi pantalla de pregunta", JOptionPane.DEFAULT_OPTION,
+	            JOptionPane.QUESTION_MESSAGE, null, mediosDePago, mediosDePago[0]);
+    	
+    	if(pago==0) {
+    		JOptionPane.showMessageDialog(null,"En 48 horas iremos a su domicilio a retirar el dinero");
+    	}else {
+    		JOptionPane.showMessageDialog(null,"Datos de la cuenta a depositar: ");
+    		JOptionPane.showMessageDialog(null,"CBU: 002314456987");
+    		JOptionPane.showMessageDialog(null,"Titular: Amore Pasta S.A");
+    		JOptionPane.showMessageDialog(null,"Banco: Banco Patagonia");
+    	}
+    	
+    	
     }
     
 }
