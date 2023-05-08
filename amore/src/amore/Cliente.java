e45011f4a41345d00f14468acef7628c465c5b25
vickaval <131685935+vickaval@users.noreply.github.com>
2023-05-08 02:22:59 -0300

    martina

--- amore/src/amore/Cliente.java
+++ amore/src/amore/Cliente.java
@@ -18,7 +18,7 @@ public class Cliente extends Usuario{
 		this.cuit = cuit;
 		this.razonSocial = razonSocial;
 		this.condicionIva = condicionIva;
-		this.clientes = new ArrayList<>();
+		
 	}
 
 		//getters y setters
@@ -63,8 +63,52 @@ public class Cliente extends Usuario{
 	        System.out.println("Bienvenido " + getNombre() +  " seleccione lo que va a pedir y la cantidad: ");
 	        
 	    }
+	
+	
+	//metodos de martina
+	
+	 public void seleccionarProducto(ArrayList<Producto> lista,Pedido p) {    
+    	for (int i = 0; i < lista.size(); i++) {
+			System.out.println(lista.get(i).getNombre());
+			System.out.println(lista.get(i).getPrecio());
+		}
+    	
+    	 Object[] productos = lista.toArray();
+    	 int cantidad=0;
+    	 double total=0;
+    	 int opciones=JOptionPane.showOptionDialog(null, "Elija una opcion", "Mi pantalla de pregunta", JOptionPane.DEFAULT_OPTION,
+ 	            JOptionPane.QUESTION_MESSAGE, null, productos, productos[0]);
+    	 if(opciones<productos.length) {
+    		 cantidad=Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la cantidad a llevar: "));
+    		 if(cantidad<=lista.get(opciones).getStockDisponible()) {
+    			 total=lista.get(opciones).getPrecio()*cantidad;
+    			 JOptionPane.showMessageDialog(null, "El total de la compra es: "+total);
+    			
+    		 }else {
+    			 JOptionPane.showMessageDialog(null, "Stock insuficiente");
+    		 }
+    	 }
+    	
+    	
+    }
+    
+    public void metodosDePago(Pedido p) {
+    	String []mediosDePago= {"Efectivo", "Transferencia"};
+    	int pago = JOptionPane.showOptionDialog(null, "Elija un medio de pago", "Mi pantalla de pregunta", JOptionPane.DEFAULT_OPTION,
+	            JOptionPane.QUESTION_MESSAGE, null, mediosDePago, mediosDePago[0]);
+    	
+    	if(pago==0) {
+    		JOptionPane.showMessageDialog(null,"En 48 horas iremos a su domicilio a retirar el dinero");
+    	}else {
+    		JOptionPane.showMessageDialog(null,"Datos de la cuenta a depositar: ");
+    		JOptionPane.showMessageDialog(null,"CBU: 002314456987");
+    		JOptionPane.showMessageDialog(null,"Titular: Amore Pasta S.A");
+    		JOptionPane.showMessageDialog(null,"Banco: Banco Patagonia");
+    	}
+    	
+    	
+    }
 	    
 	}
-
 
 
