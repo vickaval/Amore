5635ddc7f6d7a80822f721359b0aab8bd7b173b8
vickaval <131685935+vickaval@users.noreply.github.com>
2023-05-08 02:21:13 -0300

    metodo estadisticas, sebas

--- amore/src/amore/Almacenista.java
+++ amore/src/amore/Almacenista.java
@@ -2,84 +2,97 @@ package amore;
 
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 public class Almacenista extends Usuario {
-	
 
-	ArrayList<Producto>productos;
-	ArrayList<Almacenista>almacenistas;
+	ArrayList<Producto> productos;
+	ArrayList<Almacenista> almacenistas;
 
-	//clase hecha por Sebastian
-		
-	
+	private ArrayList<Producto> paraEntregar;
+	private ArrayList<Pedido> preparados;
+
+	// clase hecha por Sebastian
 
 	public Almacenista(String nombre, String apellido, String id, String nombreArea, int aniosAntiguedad, int telefono,
-				String usuario, String contrasena, double sueldo) {
-			super(nombre, apellido, id, nombreArea, aniosAntiguedad, telefono, usuario, contrasena, sueldo);
-			this.almacenistas = new ArrayList<>();
-			this.productos = new ArrayList<>();
+			String usuario, String contrasena, double sueldo) {
+		super(nombre, apellido, id, nombreArea, aniosAntiguedad, telefono, usuario, contrasena, sueldo);
+		this.almacenistas = new ArrayList<>();
+		this.productos = new ArrayList<>();
+
+		this.paraEntregar = new ArrayList<>();
+		this.preparados = new ArrayList<>();
+	}
+
+	public void estadisticas (){
+		int cont = 0;
+	
+		for (int i=1; i<= this.paraEntregar.size(); i++){
+			if(paraEntregar.equals(getParaEntregar())) {
+				cont = cont + 1;
+			}		
 		}
-	
-	
-    /*
-	public Almacenista(){;
-
-	}
-	*/
-
-
-	public boolean iniciarSesion(String usuario, String contrasena) {
-	    // Verificar si el usuario y la contrase a son v lidos
-	    if (usuario.equals(getUsuario()) &&contrasena.equals(getContraseña())) {
-	        System.out.println("Inicio de sesion exitoso.");
-	        return true;
-	    } else {
-	        System.out.println("Inicio de sesi n fallido. Por favor verifique su usuario, contrase a y permiso.");
-	        return false;
-	    }
+		if(cont >= 100) {
+			JOptionPane.showMessageDialog(null, "Producto vendido mas de 100 veces " + getParaEntregar() );
+			System.out.println("Producto vendido mas de 100 veces " + getParaEntregar());
+		} else {
+			JOptionPane.showMessageDialog(null, "El producto fue vendido menos de 100 veces " + getParaEntregar() );
+			System.out.println("El producto fue vendido menos de 100 veces " + getParaEntregar());
+		}
+					
 	}
 
+	
 
-
-	public void visualizarStockProductos (Producto p){
-	    boolean tieneStock= productos.contains(p);//chequear
-	    if(tieneStock){
-	        System.out.println(p.getNombre());
-	        System.out.println(p.getIdProducto());
-	        System.out.println(p.getStockDisponible());
-	    }else{
-	        System.out.println("No hay stock");
-	    }
+	public void visualizarStockProductos(Producto p) {
+		boolean tieneStock = productos.contains(p);// chequear
+		if (tieneStock) {
+			System.out.println(p.getNombre());
+			System.out.println(p.getIdProducto());
+			System.out.println(p.getStockDisponible());
+		} else {
+			System.out.println("No hay stock");
+		}
 	}
-	
-	
 
 	public ArrayList<Producto> getProductos() {
-	    return productos;
+		return productos;
 	}
 
 	public void setProductos(ArrayList<Producto> productos) {
-	    this.productos = productos;
+		this.productos = productos;
 	}
 
-	public void agregarProducto (Producto p){
-	    productos.add(p);//chequear
+	public void agregarProducto(Producto p) {
+		productos.add(p);// chequear
 	}
 
-	public void eliminarProducto(Producto p){
-	    productos.remove(p);
+	public void eliminarProducto(Producto p) {
+		productos.remove(p);
 	}
-
 
 	public ArrayList<Almacenista> getAlmacenistas() {
 		return almacenistas;
 	}
+
 	public void setAlmacenistas(ArrayList<Almacenista> almacenistas) {
 		this.almacenistas = almacenistas;
 	}
 
+	public ArrayList<Producto> getParaEntregar() {
+		return paraEntregar;
+	}
 
-		
+	public void setParaEntregar(ArrayList<Producto> paraEntregar) {
+		this.paraEntregar = paraEntregar;
+	}
 
-	
+	public ArrayList<Pedido> getPreparados() {
+		return preparados;
+	}
+
+	public void setPreparados(ArrayList<Pedido> preparados) {
+		this.preparados = preparados;
+	}
 
 }
