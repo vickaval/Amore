49deba815c18916f6563284c4f7f4be655fd974f
vickaval <131685935+vickaval@users.noreply.github.com>
2023-05-16 01:02:39 -0300

    Update Almacenista.java

--- amore/src/amore/Almacenista.java
+++ amore/src/amore/Almacenista.java
@@ -1,98 +1,126 @@
-package amore;
+package ejemplo;
 
-import java.util.ArrayList;
+	public class Almacenista extends Usuario {
 
-import javax.swing.JOptionPane;
+		private String nombreArea;
+		private int aniosAntiguedad;
+		private double sueldo;
+		private int idDepo;
+		/*
+		ArrayList<Producto> productos;
+		ArrayList<Almacenista> almacenistas;
 
-public class Almacenista extends Usuario {
+		private ArrayList<Producto> paraEntregar;
+		private ArrayList<Pedido> preparados;
+		*/
 
-	ArrayList<Producto> productos;
-	ArrayList<Almacenista> almacenistas;
+		// clase hecha por Sebastian
 
-	private ArrayList<Producto> paraEntregar;
-	private ArrayList<Pedido> preparados;
+		public Almacenista(String nombre, String apellido, String id, String usuario, int telefono, String contraseña,
+				String nombreArea, int aniosAntiguedad, double sueldo, int idDepo)  {
+			super(nombre, apellido, id, usuario, telefono, contraseña);
+			this.nombreArea = nombreArea;
+			this.aniosAntiguedad = aniosAntiguedad;
+			this.sueldo = sueldo;
+			this.idDepo = idDepo;
 
-	// clase hecha por Sebastian
-
-	public Almacenista(String nombre, String apellido, String id, String nombreArea, int aniosAntiguedad, int telefono,
-			String usuario, String contrasena, double sueldo) {
-		super(nombre, apellido, id, nombreArea, aniosAntiguedad, telefono, usuario, contrasena, sueldo);
-		this.almacenistas = new ArrayList<>();
-		this.productos = new ArrayList<>();
-
-		this.paraEntregar = new ArrayList<>();
-		this.preparados = new ArrayList<>();
-	}
-
-	public void estadisticas (){
-		int cont = 0;
-	
-		for (int i=1; i<= this.paraEntregar.size(); i++){
-			if(paraEntregar.equals(getParaEntregar())) {
-				cont = cont + 1;
-			}		
 		}
-		if(cont >= 100) {
-			JOptionPane.showMessageDialog(null, "Producto vendido mas de 100 veces " + getParaEntregar() );
-			System.out.println("Producto vendido mas de 100 veces " + getParaEntregar());
-		} else {
-			JOptionPane.showMessageDialog(null, "El producto fue vendido menos de 100 veces " + getParaEntregar() );
-			System.out.println("El producto fue vendido menos de 100 veces " + getParaEntregar());
+		
+		
+
+		public String getNombreArea() {
+			return nombreArea;
 		}
-					
-	}
 
-	
-
-	public void visualizarStockProductos(Producto p) {
-		boolean tieneStock = productos.contains(p);// chequear
-		if (tieneStock) {
-			System.out.println(p.getNombre());
-			System.out.println(p.getIdProducto());
-			System.out.println(p.getStockDisponible());
-		} else {
-			System.out.println("No hay stock");
+		public void setNombreArea(String nombreArea) {
+			this.nombreArea = nombreArea;
 		}
-	}
 
-	public ArrayList<Producto> getProductos() {
-		return productos;
-	}
+		public int getAniosAntiguedad() {
+			return aniosAntiguedad;
+		}
 
-	public void setProductos(ArrayList<Producto> productos) {
-		this.productos = productos;
-	}
+		public void setAniosAntiguedad(int aniosAntiguedad) {
+			this.aniosAntiguedad = aniosAntiguedad;
+		}
 
-	public void agregarProducto(Producto p) {
-		productos.add(p);// chequear
-	}
+		public double getSueldo() {
+			return sueldo;
+		}
 
-	public void eliminarProducto(Producto p) {
-		productos.remove(p);
-	}
+		public void setSueldo(double sueldo) {
+			this.sueldo = sueldo;
+		}
+		
+		
+		public int getIdDepo() {
+			return idDepo;
+		}
+		public void setIdDepo(int idDepo) {
+			this.idDepo = idDepo;
+		}
+		
 
-	public ArrayList<Almacenista> getAlmacenistas() {
-		return almacenistas;
-	}
 
-	public void setAlmacenistas(ArrayList<Almacenista> almacenistas) {
-		this.almacenistas = almacenistas;
-	}
 
-	public ArrayList<Producto> getParaEntregar() {
-		return paraEntregar;
-	}
+		//metodos
+		public boolean agregarAlmacenista() {    
+			String sql ="INSERT INTO `almacenista`(`id`, `nombre`, `apellido`, `nombreArea`, `aniosAntiguedad`, `telefono`, `contraseña`, `sueldo`,`usuario`, `idDepo`) VALUES (?,?,?,?,?,?,?,?, ?,?) ";
+			
+			try {
+				stmt = conexion.prepareStatement(sql);
+				stmt.setString(1, this.getId());
+				stmt.setString(2, this.getNombre());
+				stmt.setString(3, this.getApellido());
+				stmt.setString(4, this.getNombreArea());
+				stmt.setLong(5, this.getAniosAntiguedad());
+				stmt.setLong(6, this.getTelefono());
+				stmt.setString(7, this.getContraseña());					
+				stmt.setDouble(8, this.getSueldo());
+				stmt.setString(9, this.getUsuario());	
+				stmt.setLong(10, this.getIdDepo());
+				stmt.executeUpdate();
+				return true;
+				
+			} catch (Exception e) {
+				System.out.println("Hubo un error"+e.getMessage());
+				return false;
+			}
+		}
+		
+		
+		/*
+		public void estadisticas (){
+			int cont = 0;
+		
+			for (int i=1; i<= this.paraEntregar.size(); i++){
+				if(paraEntregar.equals(getParaEntregar())) {
+					cont = cont + 1;
+				}		
+			}
+			if(cont >= 100) {
+				JOptionPane.showMessageDialog(null, "Producto vendido mas de 100 veces " + getParaEntregar() );
+				System.out.println("Producto vendido mas de 100 veces " + getParaEntregar());
+			} else {
+				JOptionPane.showMessageDialog(null, "El producto fue vendido menos de 100 veces " + getParaEntregar() );
+				System.out.println("El producto fue vendido menos de 100 veces " + getParaEntregar());
+			}
+						
+		}		
 
-	public void setParaEntregar(ArrayList<Producto> paraEntregar) {
-		this.paraEntregar = paraEntregar;
-	}
+		public void visualizarStockProductos(Producto p) {
+			boolean tieneStock = productos.contains(p);// chequear
+			if (tieneStock) {
+				System.out.println(p.getNombre());
+				System.out.println(p.getIdProducto());
+				System.out.println(p.getStockDisponible());
+			} else {
+				System.out.println("No hay stock");
+			}
+		}
+		
+		*/
 
-	public ArrayList<Pedido> getPreparados() {
-		return preparados;
-	}
-
-	public void setPreparados(ArrayList<Pedido> preparados) {
-		this.preparados = preparados;
-	}
 
 }
+
