8521edc2ede55ff48dfd033280fec45235234b6f
Enrique Arroyo <107654724+EnriqueArroyo@users.noreply.github.com>
2023-06-03 18:38:49 -0300

    1er Merge

---
+++ amore/src/Logica/Salsa.java
@@ -0,0 +1,11 @@
+package logica;
+
+public class Salsa extends Producto {
+
+    public Salsa(int idProducto, String nombre, String tipo, double precio, int cantidad) {
+        super(idProducto, nombre, tipo, precio, cantidad);
+    }
+
+
+}
+
