eb716a4ad11dfbdc65c4ccda34bdc82cfece3fe6
Enrique Arroyo <107654724+EnriqueArroyo@users.noreply.github.com>
2023-06-03 18:29:28 -0300

    1er Merge

---
+++ amore/src/Logica/Queso.java
@@ -0,0 +1,13 @@
+package logica;
+
+public class Queso extends Producto {
+
+    public Queso(int idProducto, String nombre, String tipo, double precio, int cantidad) {
+        super(idProducto, nombre, tipo, precio, cantidad);
+    }
+
+    
+
+
+}
+
