f68c70055baef258fdae791ee5b1984d01034334
MartinaAsad <116227485+MartinaAsad@users.noreply.github.com>
2023-06-11 20:37:04 -0300

    Add files via upload

---
+++ amore (7).sql
@@ -0,0 +1,438 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-06-2023 a las 01:36:45
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `amore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `administrador`
+--
+
+CREATE TABLE `administrador` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellido` varchar(45) DEFAULT NULL,
+  `nombreArea` varchar(45) DEFAULT NULL,
+  `aniosAntiguedad` int(11) DEFAULT NULL,
+  `telefono` int(11) DEFAULT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `contraseña` varchar(45) DEFAULT NULL,
+  `sueldo` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `administrador`
+--
+
+INSERT INTO `administrador` (`id`, `nombre`, `apellido`, `nombreArea`, `aniosAntiguedad`, `telefono`, `usuario`, `contraseña`, `sueldo`) VALUES
+(1, 'victoria', 'valerio', 'administrador', 7, 1234567891, 'administrador', '12345678', '302578');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `almacenista`
+--
+
+CREATE TABLE `almacenista` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellido` varchar(45) DEFAULT NULL,
+  `nombreArea` varchar(45) DEFAULT NULL,
+  `aniosAntiguedad` int(11) DEFAULT NULL,
+  `telefono` int(11) DEFAULT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `contraseña` varchar(45) DEFAULT NULL,
+  `sueldo` varchar(45) DEFAULT NULL,
+  `idDepo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `almacenista`
+--
+
+INSERT INTO `almacenista` (`id`, `nombre`, `apellido`, `nombreArea`, `aniosAntiguedad`, `telefono`, `usuario`, `contraseña`, `sueldo`, `idDepo`) VALUES
+(1, 'pablo', 'patos', 'repositor', 5, 46546565, 'almacenista', '12345pablo', '103458.0', 1),
+(2, 'ramiro', 'lertora', 'pedidos', 4, 456987321, 'almacenista', '546546rama', '102458.0', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoriaproducto`
+--
+
+CREATE TABLE `categoriaproducto` (
+  `idCategoria` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL,
+  `razonSocial` varchar(45) DEFAULT NULL,
+  `condicionIVA` varchar(11) DEFAULT NULL,
+  `cuit` int(11) DEFAULT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellido` varchar(45) DEFAULT NULL,
+  `telefono` int(12) DEFAULT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `contraseña` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id`, `razonSocial`, `condicionIVA`, `cuit`, `nombre`, `apellido`, `telefono`, `usuario`, `contraseña`) VALUES
+(4, 'estrella srl', 'exento', 45123658, 'marcela', 'flores', 11235689, 'cliente', 'florsita');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `deposito`
+--
+
+CREATE TABLE `deposito` (
+  `idDepo` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `materiaprima`
+--
+
+CREATE TABLE `materiaprima` (
+  `idMp` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `procedencia` varchar(45) DEFAULT NULL,
+  `precio` double DEFAULT NULL,
+  `stockDisponible` int(11) DEFAULT NULL,
+  `idDepo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `operario`
+--
+
+CREATE TABLE `operario` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellido` varchar(45) DEFAULT NULL,
+  `nombreArea` varchar(45) DEFAULT NULL,
+  `aniosAntiguedad` int(11) DEFAULT NULL,
+  `telefono` int(11) DEFAULT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `contraseña` varchar(45) DEFAULT NULL,
+  `sueldo` varchar(45) DEFAULT NULL,
+  `turno` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `operario`
+--
+
+INSERT INTO `operario` (`id`, `nombre`, `apellido`, `nombreArea`, `aniosAntiguedad`, `telefono`, `usuario`, `contraseña`, `sueldo`, `turno`) VALUES
+(1, 'claudio', 'pepper', 'produccion', 6, 1125478963, 'operario', '1234567clau', '125478.0', 'mañana'),
+(2, 'greta', 'panser', 'mezcla', 4, 1125478693, 'operario', '12345698gret', '102458.0', 'noche'),
+(3, 'pedro', 'carlos', 'repositor', 6, 6546546, 'operario', '5454carlos', '103456.0', 'tarde'),
+(4, 'matias', 'robledo', 'produccion', 5, 1234569871, 'operario', '123456ma', '102453.0', 'mañana'),
+(8, 'martina', 'asad', 'produccion', 5, 41023658, 'operario', 'martina123', '456321.0', 'mañana');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `idPedido` int(11) NOT NULL,
+  `descuento` decimal(10,0) DEFAULT NULL,
+  `total` decimal(10,0) DEFAULT NULL,
+  `formaPago` varchar(11) DEFAULT NULL,
+  `idVendedor` int(11) NOT NULL,
+  `idCliente` int(11) NOT NULL,
+  `estado` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `produccion`
+--
+
+CREATE TABLE `produccion` (
+  `idProduccion` int(11) NOT NULL,
+  `fechaProduccion` datetime DEFAULT NULL,
+  `totalProducido` int(11) DEFAULT NULL,
+  `produccioncol` varchar(45) DEFAULT NULL,
+  `idOperario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `idProducto` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `precio` decimal(10,0) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `idDepo` int(11) NOT NULL,
+  `idProduccion` int(11) NOT NULL,
+  `idCategoria` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`idProducto`, `nombre`, `precio`, `cantidad`, `idDepo`, `idProduccion`, `idCategoria`) VALUES
+(1, 'Espaguetis', 6, 50, 1, 1, 1),
+(2, 'Fusilli', 5, 40, 1, 1, 1),
+(3, 'Salsa de tomate', 3, 100, 2, 1, 2),
+(4, 'Salsa Alfredo', 4, 80, 2, 1, 2),
+(5, 'Queso Parmesano', 8, 30, 3, 1, 3),
+(6, 'Queso Mozzarella', 7, 35, 3, 1, 3),
+(7, '', 0, 0, 0, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `cuit` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `montoComprado` decimal(10,0) DEFAULT NULL,
+  `deuda` decimal(9,2) NOT NULL,
+  `materialesQueProvee` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `proveedor`
+--
+
+INSERT INTO `proveedor` (`cuit`, `nombre`, `montoComprado`, `deuda`, `materialesQueProvee`) VALUES
+(12536, 'alda', 14589, 0.00, 'fiambre'),
+(12345, 'esmeralda', 25789, 1200.00, 'harina'),
+(12345, 'manolete', 25478, 20145.00, 'harina'),
+(12345, 'ramona', 12546, 1200.00, 'tomate');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor_x_materiaprima`
+--
+
+CREATE TABLE `proveedor_x_materiaprima` (
+  `proveedor_cuit` int(11) NOT NULL,
+  `materiaPrima_codigoMp` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vendedor`
+--
+
+CREATE TABLE `vendedor` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellido` varchar(45) DEFAULT NULL,
+  `nombreArea` varchar(45) DEFAULT NULL,
+  `aniosAntiguedad` int(11) DEFAULT NULL,
+  `telefono` int(11) DEFAULT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `contraseña` varchar(45) DEFAULT NULL,
+  `sueldo` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `vendedor`
+--
+
+INSERT INTO `vendedor` (`id`, `nombre`, `apellido`, `nombreArea`, `aniosAntiguedad`, `telefono`, `usuario`, `contraseña`, `sueldo`) VALUES
+(1, 'alda', 'rivera', 'ventas', 4, 456789123, 'vendedor', '1234alda', '102345.0'),
+(2, 'julio', 'aguirre', 'ventas', 5, 1234567897, 'vendedor', '12345', '123456.0'),
+(3, 'pablo', 'esquivel', 'ventas', 9, 1234567891, 'vendedor', '12345', '123789.0');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `almacenista`
+--
+ALTER TABLE `almacenista`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `categoriaproducto`
+--
+ALTER TABLE `categoriaproducto`
+  ADD PRIMARY KEY (`idCategoria`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `deposito`
+--
+ALTER TABLE `deposito`
+  ADD PRIMARY KEY (`idDepo`);
+
+--
+-- Indices de la tabla `materiaprima`
+--
+ALTER TABLE `materiaprima`
+  ADD PRIMARY KEY (`idMp`);
+
+--
+-- Indices de la tabla `operario`
+--
+ALTER TABLE `operario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`idPedido`);
+
+--
+-- Indices de la tabla `produccion`
+--
+ALTER TABLE `produccion`
+  ADD PRIMARY KEY (`idProduccion`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`idProducto`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`nombre`);
+
+--
+-- Indices de la tabla `proveedor_x_materiaprima`
+--
+ALTER TABLE `proveedor_x_materiaprima`
+  ADD PRIMARY KEY (`proveedor_cuit`,`materiaPrima_codigoMp`);
+
+--
+-- Indices de la tabla `vendedor`
+--
+ALTER TABLE `vendedor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `almacenista`
+--
+ALTER TABLE `almacenista`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `categoriaproducto`
+--
+ALTER TABLE `categoriaproducto`
+  MODIFY `idCategoria` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `deposito`
+--
+ALTER TABLE `deposito`
+  MODIFY `idDepo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `operario`
+--
+ALTER TABLE `operario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `idPedido` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `produccion`
+--
+ALTER TABLE `produccion`
+  MODIFY `idProduccion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `vendedor`
+--
+ALTER TABLE `vendedor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
