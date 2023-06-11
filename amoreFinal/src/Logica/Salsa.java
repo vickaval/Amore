be4d7ac2c9126e3cafafd010b56fbb3748b3c767
vickaval <131685935+vickaval@users.noreply.github.com>
2023-06-11 03:08:32 -0300

    -

---
+++ amoreFinal/src/Logica/Salsa.java
@@ -0,0 +1,9 @@
+package Logica;
+
+public class Salsa extends Producto {
+	
+	 public Salsa(int idProducto, String nombre, double precio, int cantidad, int idDepo, int idProduccion, String tipo) {
+	        super(idProducto, nombre, precio, cantidad, idDepo, idProduccion, tipo);
+	    }
+
+}
