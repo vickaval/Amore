08a2659da908fe019b7c2007b890886101573e40
MartinaAsad <lasupermartu15@gmail.com>
2023-06-12 00:32:31 -0300

    elimine clases

--- Logica/Queso.java
+++
@@ -1,9 +0,0 @@
-package Logica;
-
-public class Queso extends Producto {
-	
-	 public Queso(int idProducto, String nombre, double precio, int cantidad, int idDepo, int idProduccion, String tipo) {
-	        super(idProducto, nombre, precio, cantidad, idDepo, idProduccion, tipo);
-	    }
-
-}
--- Logica/Salsa.java
+++
@@ -1,9 +0,0 @@
-package Logica;
-
-public class Salsa extends Producto {
-	
-	 public Salsa(int idProducto, String nombre, double precio, int cantidad, int idDepo, int idProduccion, String tipo) {
-	        super(idProducto, nombre, precio, cantidad, idDepo, idProduccion, tipo);
-	    }
-
-}
