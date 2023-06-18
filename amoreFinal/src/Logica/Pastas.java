91858af9b95a93c1a5acddcd38194c72792f8642
vickaval <131685935+vickaval@users.noreply.github.com>
2023-06-18 14:59:30 -0300

    -

---
+++ amoreFinal/src/Logica/Pastas.java
@@ -0,0 +1,17 @@
+package Logica;
+
+public class Pastas extends Producto {
+
+
+
+	public Pastas(int idProducto, String nombre, String tipo, double precio, int stockDisponible, int idDepo,
+			int idProduccion, int idCategoria, int cantidad) {
+		super(idProducto, nombre, tipo, precio, stockDisponible, idDepo, idProduccion, idCategoria, cantidad);
+
+	}
+	
+	 
+
+
+	
+}
